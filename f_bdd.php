f66aa245a1d7306e986df7dca2ebbe654f591687
Molenat <66407938+Molenat@users.noreply.github.com>
2020-06-10 10:34:25 +0200

    Add files via upload

---
+++ f_bdd.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+function connexionBDD()
+{
+	try
+	{
+            $bdd = new PDO('mysql:host=localhost;dbname=bdd_geststages;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            return $bdd;
+	}
+	catch(Exception $e)
+	{
+		$pdo_error = $e->getMessage();
+                return false;
+	}
+    
+}
+
+?>
